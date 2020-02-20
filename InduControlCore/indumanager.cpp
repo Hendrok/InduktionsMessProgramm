b450d3b287643317551f1b9644e3917ab103f4d7
hendrok <hendrikloehr1995@gmail.com>
2020-02-20 14:20:22 +0100

    test linux2

--- InduControlCore/indumanager.cpp
+++ InduControlCore/indumanager.cpp
@@ -12,7 +12,7 @@
 #include "../Instruments/ppmsdatapoint.h"
 InduManager::InduManager()
     : measurementNumber_(0)
-    //, mVecSeq_ (std::vector <std::make_shared<MeasurementSequence>>()) // wie geht diese
+    //, mVecSeq_ (std::vector <std::make_shared<MeasurementSequence>>()) // wie geht dieses
     , instrumentmanager_ (std::make_unique<InstrumentManager>())
     , fw_(nullptr)
     , mSeqTc_(std::make_shared <MeasSeqTc>())
